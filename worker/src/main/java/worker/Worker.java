4861810df16402e265c50b00e1c758dd22f22374
setayeshghamat <145782254+setayeshghamat@users.noreply.github.com>
2024-02-19 16:24:19 +0100

    db step

--- worker/src/main/java/worker/Worker.java
+++ worker/src/main/java/worker/Worker.java
@@ -8,8 +8,8 @@ import org.json.JSONObject;
 class Worker {
   public static void main(String[] args) {
     try {
-        Jedis redis = connectToRedis("127.0.0.1");
-        Connection dbConn = connectToDB("127.0.0.1");
+        Jedis redis = connectToRedis("redis");
+        Connection dbConn = connectToDB("db");
 
         System.err.println("Watching vote queue");
 
@@ -23,6 +23,7 @@ class Worker {
             updateVote(dbConn, voterID, vote);
         }
     } catch (SQLException e) {
+      
       e.printStackTrace();
       System.exit(1);
     }
